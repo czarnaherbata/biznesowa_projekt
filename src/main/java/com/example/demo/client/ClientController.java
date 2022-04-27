28b077e82d628803ab9ebcde6cdb5c708be4d142
czarnaherbata <czarnaherbata223@gmail.com>
2022-04-27 16:44:14 +0200

    creating basic service

--- src/main/java/com/example/demo/client/ClientController.java
+++ src/main/java/com/example/demo/client/ClientController.java
@@ -1,5 +1,6 @@
 package com.example.demo.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,18 +10,16 @@ import java.util.List;
 @RestController
 @RequestMapping(path="api/v1/client")
 public class ClientController {
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = new ClientService();
+    }
+
     @GetMapping
     public List<Client> getStudents()
     {
-
-        return List.of(
-                new Client(
-                        1L,
-                        "Zuzia",
-                        "Baginska",
-                        "czarnaherbata223@gmail.com",
-                        "01.01.2002"
-                )
-        );
+return clientService.getStudents();
     }
 }
